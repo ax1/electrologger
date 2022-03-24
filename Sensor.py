569cbe369092de619105fd943079e1292df6f186
Angel Rego <angelrego@gmail.com>
2022-03-24 11:21:35 +0100

    sensor: container for type based logic anomalies and data

---
+++ Sensor.py
@@ -0,0 +1,19 @@
+import time
+
+
+class Sensor:
+
+    '''
+    Sensors are elements that produce data.
+    They usually belong to one device,
+    but the behaviour (state) for data generation is here.
+    '''
+
+   def __init__(self, source, type, anomaly) -> None:
+        self.source = source
+        self.type = type
+        self.anomaly = anomaly
+        self.timestamp = round(time.time() * 1000)
+
+    def run(self):
+        return 'create line'
