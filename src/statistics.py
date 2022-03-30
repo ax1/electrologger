bad2b7ef921c424c48150c47bfbebd21cd0a7a79
Angel Rego <angelrego@gmail.com>
2022-03-30 15:15:24 +0200

    sandbox: add example test for diferent statistical functions

--- src/statistics.py
+++ src/statistics.py
@@ -4,15 +4,36 @@ https://numpy.org/doc/stable/reference/random/generated/numpy.random.Generator.n
 import matplotlib.pyplot as plt
 import numpy as np
 
-# NORMAL DISTRIBUTION
-mu, sigma = 0, 0.1
-dist_normal = np.random.default_rng().normal(mu, sigma, 1000)
-dist_gamma = np.random.standard_gamma(2, 1)
-count, bins, ignored = plt.hist(dist_normal, 30, density=True)
+# ------------- NORMAL DISTRIBUTION------------------------
+mu, sigma = 10, 0.1
+size = 100
+normal = np.random.default_rng().normal(mu, sigma, size)
+
+# get random val ()
+#  when infinite values required on the same curve
+print('')
+for r in range(10):
+    print(np.random.choice(normal))
+
+# when finite values required, this is faster
+print('')
+for r in range(size):
+    print(normal[r])
+
+
+# walk over the curve
+print('')
+curve = np.sort(normal)
+for r in range(size):
+    print(curve[r])
+
+# plot distribution
+count, bins, ignored = plt.hist(normal, 30, density=True)
 plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu)
          ** 2 / (2 * sigma**2)),         linewidth=2, color='r')
 plt.show()
 
-# GAMMA DISTRIBUTION
+# ---------------GAMMA DISTRIBUTION-------------------------
+dist_gamma = np.random.standard_gamma(2, 1)
 shape, scale = 2., 1.  # mean and width
 s = np.random.default_rng().standard_gamma(shape, 1000000)
