7c3554fa559a2db9604fb2ef50d41bc6f07e97ca
Angel Rego <angelrego@gmail.com>
2022-04-01 11:32:07 +0200

    test: add sensor test to check normal distribution

--- test/test.py
+++ test/test.py
@@ -1,4 +1,6 @@
 from src.probe import *
+from src.sensor import *
+import matplotlib.pyplot as plt
 
 
 def test_probe():
@@ -12,4 +14,18 @@ def test_oscillator():
     print()
 
 
-test_oscillator()
+def testSensor():
+    sensor = Sensor('boiler-672,power,normal')
+    arr = []
+    for r in range(1000):
+        sensor.run()
+        arr.append(sensor.value1)
+    narr = np.array(arr)
+    #plt.hist(narr, bins=50, density=True)
+    (n, bins) = np.histogram(narr, bins=10, density=True)
+    plt.plot(.5 * (bins[1:] + bins[:-1]), n)
+    plt.show()
+
+
+# test_oscillator()
+testSensor()
