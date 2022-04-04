98af66f43f7dc7d629e1380e4870f39f9ad7c040
Angel Rego <angelrego@gmail.com>
2022-04-04 09:53:12 +0200

    test: gui, add a distribution+timeline charts in onw window.
    WHile the main chart is the shape of the distribution, it is also interesting to check the real behaviour of the sensors, so in the test we can display now if the sensor shows a normal distribution but also if the sensor shows an apparently random behaviour

--- test/test.py
+++ test/test.py
@@ -14,18 +14,32 @@ def test_oscillator():
     print()
 
 
-def testSensor():
+def test_sensor():
     sensor = Sensor('boiler-672,power,normal')
     arr = []
-    for r in range(1000):
-        sensor.run()
+    timestamp = 0
+    for r in range(2000):
+        timestamp += 1
+        sensor.run(timestamp)
         arr.append(sensor.value1)
     narr = np.array(arr)
+
     #plt.hist(narr, bins=50, density=True)
     (n, bins) = np.histogram(narr, bins=10, density=True)
-    plt.plot(.5 * (bins[1:] + bins[:-1]), n)
+    fig, axd = plt.subplot_mosaic(
+        [['1', '2'], ['3', '3']], layout='constrained')
+    axd['1'].plot(.5 * (bins[1:] + bins[:-1]), n)
+    axd['3'].plot(narr)
+    plt.show()
+
+
+def test_plot():
+    fig, axd = plt.subplot_mosaic(
+        [['1', '2'], ['3', '2']], layout='constrained')
+    axd['1'].set_title('1')
     plt.show()
 
 
 # test_oscillator()
-testSensor()
+test_sensor()
+# test_plot()
