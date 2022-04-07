96f4336bc96f883cfd809cd344835c28d4b13855
Angel Rego <angelrego@gmail.com>
2022-04-07 18:42:59 +0200

    anomaly: increase the random life of the anomaly
    This value will be switch back to previous values once detection is tested. For now, the anomaly, if started, is so long that it will affect heavily data in the whole file for that sensor.

--- src/anomaly.py
+++ src/anomaly.py
@@ -6,7 +6,7 @@ class Anomaly:
     def __init__(self, type):
         self.counter = 0
         self.type = type
-        self.life = random.randint(20, 200)
+        self.life = random.randint(2000, 4000)
         self.a = 1  # multiplier. Eg: f(x)=2x+4 -> g(x)=(sigma*2)x+4
         self.b = 0  # addition.Eg: f(x)=2x+4 -> g(x)=2x+mean*4
         if(type == 'anomaly_sigma'):
